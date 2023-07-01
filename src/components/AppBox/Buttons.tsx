6d23fcd33657a0093ac8e344c27ac320e4a7e44a
X-Tripen <bnhp@op.pl>
2023-07-01 09:43:41 +0200

    !feat:add buttons component

---
+++ src/components/AppBox/Buttons.tsx
@@ -0,0 +1,45 @@
+interface ButtonProps {
+  readonly children?: React.ReactNode;
+  readonly onClick?: () => void;
+  readonly type?: "button" | "submit" | "reset" | undefined;
+  readonly primary?: boolean;
+  readonly lang?: boolean;
+  readonly disabled?: boolean;
+  readonly search?: boolean;
+}
+
+function Button({
+  children,
+  type,
+  onClick,
+  primary,
+  disabled,
+  search,
+}: ButtonProps): JSX.Element {
+  let classes;
+  if (primary) {
+    classes =
+      "px-5 py-2 bg-yellow-800 bg-opacity-20 rounded-bl-lg rounded-tl-lg rounded-br-lg rounded-tr-lg duration-200 border-r-4 border-l-4 border-stone-800 text-stone-300 hover:text-stone-50 md:min-w-[217px] hover:min-w-[250px] uppercase tracking-wider";
+  }
+  if (search) {
+    classes +=
+      " h-10 text-2xl bg-stone-50 border-l-[5px] px-4 rounded-r-md hover:bg-stone-100 hover:text-teal-500 duration-200";
+  }
+  if (disabled) {
+    classes += " opacity-40 hover:translate-y-0 active:translate-y-0";
+  }
+
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={classes}
+      style={{ WebkitTapHighlightColor: "transparent" }}
+      disabled={disabled || false}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
