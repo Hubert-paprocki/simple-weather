3812f74bc5bbf8ca4c7caad952d36140c6b8771f
X-Tripen <bnhp@op.pl>
2023-07-02 22:12:40 +0200

    feat:add weather location name

--- src/components/AppBox/AppBox.tsx
+++ src/components/AppBox/AppBox.tsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import CurrentWeather from "./ContainerBox/CurrentWeather/CurrentWeather";
 import { WeatherData } from "../../App";
+
 interface AppboxProps {
-  isLoading: boolean;
   currentWeatherData: WeatherData | null;
 }
 
-function Appbox({ isLoading, currentWeatherData }: AppboxProps) {
+function Appbox({ currentWeatherData }: AppboxProps) {
   return (
     <div className="w-3/4 max-w-5xl bg-slate-50 rounded-md p-7">
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <CurrentWeather data={currentWeatherData} />
+      {currentWeatherData && (
+        <p className="mb-2 ml-1  text-xl">
+          {currentWeatherData.location.name},{" "}
+          {currentWeatherData.location.country}
+        </p>
       )}
+      <CurrentWeather data={currentWeatherData} />
     </div>
   );
 }
