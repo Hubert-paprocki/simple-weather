d17b1f248b069eeac04234c5307e1918c0cf82ae
X-Tripen <bnhp@op.pl>
2023-07-06 12:34:59 +0200

    feat:add hourly weather tile list

---
+++ src/components/AppBox/ContainerBox/HourlyWeather/HourlyWeatherList/HourlyWeatherTile.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { getWeatherIcon } from "../../ForecastWeather/ForecastWeatherTile";
+import { weatherBackgroundColor } from "../../CurrentWeather/CurrentWeather";
+
+interface HourlyWeatherTileProps {
+  data: any;
+}
+
+function HourlyWeatherTile({ data }: HourlyWeatherTileProps) {
+  let weatherIcon = null;
+
+  if (data && data.condition && data.condition.text) {
+    weatherIcon = getWeatherIcon(data.condition.text);
+  }
+
+  return (
+    <div
+      className={`${weatherBackgroundColor(
+        data && data.condition && data.condition.text ? data.condition.text : ""
+      )} rounded-md py-4 px-[1.25rem] text-slate-100 flex flex-col text-center font-semibold text-lg`}
+    >
+      <p>
+        {Math.round(data && data.temp_c)}
+        <sup>°</sup>
+      </p>
+      <div className="text-5xl">{weatherIcon}</div>
+      <p className="flex">
+        {data && data.time ? data.time.substring(10, 16) : ""}
+      </p>
+    </div>
+  );
+}
+
+export default HourlyWeatherTile;
